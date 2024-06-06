ec2a9631ed7dbf4da071d5abd48fe545d9111b7b
Muhammet Burak Golec <mozartburak42@gmail.com>
2024-06-06 03:27:21 +0300

    Add: Some user

--- init_db.py
+++ init_db.py
@@ -13,5 +13,12 @@ hashed_password = generate_password_hash('123456', method='pbkdf2:sha256')
 cur.execute("INSERT INTO user (username, email, password, is_admin) VALUES (?, ?, ?, ?)",
             ('admin', 'admin@blog.com', hashed_password, 1))
 
+cur.execute("INSERT INTO user (username, email, password, is_admin) VALUES (?, ?, ?, ?)",
+            ('kagan', 'kagan@blog.com', hashed_password, 0))
+
+cur.execute("INSERT INTO user (username, email, password, is_admin) VALUES (?, ?, ?, ?)",
+            ('ali', 'ali@blog.com', hashed_password, 0))
+
+
 connection.commit()
 connection.close()
